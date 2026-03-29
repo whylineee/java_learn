10edd853920a490b7cf695919e87b4ed7ff307c5
whylineee <msavin901001@gmail.com>
2026-03-29 22:08:43 +0300

    boolean type and operators

---
+++ src/BooleanExample_lesson3.java
@@ -0,0 +1,10 @@
+public class BooleanExample_lesson3 {
+    public static void main(String[] args) {
+        int value1 = 0;
+        int value2 = 10;
+        boolean test = value1 == 0;
+        boolean test2 = value2 > 2;
+        System.out.println(test && test2);
+
+    }
+}
---
+++ src/lesson_3.java
@@ -0,0 +1,23 @@
+public class lesson_3 {
+    public static void main(String[] args) {
+//        int value = 5;
+//        double value2 = 3;
+//        double sum = value / (double) value2;
+//        System.out.println(sum);
+
+//        int value = 54;
+//        value += 20;
+//        System.out.println((int) value);
+
+
+        int value = 10;
+//        value = value + 1;
+//        value += 1;
+        value++;
+        System.out.println(value);
+        value--;
+        System.out.println(value);
+
+
+    }
+}
