57edc0b3d1fc9ab526b5d02d918c5c4a8e8646ed
whylineee <msavin901001@gmail.com>
2026-03-29 21:35:46 +0300

    lesson 2 part 2

--- src/lesson_2.java
+++ src/lesson_2.java
@@ -1,2 +1,23 @@
 public class lesson_2 {
+    public static void main(String[] args) {
+        int temp;
+
+        temp = 567;
+        System.out.println(temp);
+
+        boolean booleanValue = true;
+        System.out.println(booleanValue);
+
+        char testValue = 'a';
+        System.out.println(testValue);
+
+        int value = 26;
+        System.out.println(value);
+
+        double value2 = 74.213;
+        System.out.println(value2);
+
+        testValue = 'f';
+        System.out.println(testValue);
+    }
 }
