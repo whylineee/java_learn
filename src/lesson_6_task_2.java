93d14b5768ef7cef6a1fc58f643b103ca515b273
whylineee <msavin901001@gmail.com>
2026-03-30 20:26:34 +0300

    tasks 1, 2

--- src/lesson_6_task_2.java
+++ src/lesson_6_task_2.java
@@ -2,7 +2,7 @@ public class lesson_6_task_2 {
     public static void main(String[] args) {
 // ------- TASK 2 -------
 //   912312
-        // ЗАКІНЧИЛИ НА 10:37
+        // ЗАКІНЧИЛИ НА 10:37, 8 урок
         int value = 9123123;
         int testValue = value % 10;
         if (testValue == 3) {
