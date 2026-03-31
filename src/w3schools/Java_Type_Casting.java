e9050f8f80d5222594437ea5ca4997ed761d2211
whylineee <msavin901001@gmail.com>
2026-03-31 22:51:42 +0300

    learn with w3schools

---
+++ src/w3schools/Java_Type_Casting.java
@@ -0,0 +1,17 @@
+package w3schools;
+
+public class Java_Type_Casting {
+    public static void main(String[] args) {
+        int myInt = 9;
+        double myDouble = myInt;
+
+        System.out.println(myDouble);
+
+    }
+}
+
+
+
+
+
+
