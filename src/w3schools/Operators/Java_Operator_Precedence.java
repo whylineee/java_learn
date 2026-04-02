654d0511a08d8dbc47a6a2e9e2ba965a7bcc0f46
whylineee <msavin901001@gmail.com>
2026-04-02 21:58:12 +0300

    ended java operators and code challenge

---
+++ src/w3schools/Operators/Java_Operator_Precedence.java
@@ -0,0 +1,18 @@
+package w3schools.Operators;
+
+public class Java_Operator_Precedence {
+    public static void main(String[] args) {
+        int result1 = 2 + 3 * 4;
+        int result2 = (2 + 3) * 4;
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+
+        int itemPrice = 100;
+        int shippingCost = 200;
+
+        int sum = itemPrice + shippingCost;
+        System.out.println(sum);
+    }
+}
