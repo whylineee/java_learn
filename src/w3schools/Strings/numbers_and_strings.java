63c8424409b3da963ca934403a9af09d77a67b3a
whylineee <msavin901001@gmail.com>
2026-04-03 22:13:09 +0300

    end learn operators

---
+++ src/w3schools/Strings/numbers_and_strings.java
@@ -0,0 +1,17 @@
+package w3schools.Strings;
+
+public class numbers_and_strings {
+    public static void main(String[] args) {
+//        int x = 10213;
+//        int y = 21230;
+//        int z = x + y;
+//
+
+//
+        String x = "10";
+        String y = "20";
+        String z = x + y;
+        System.out.println(z);
+
+    }
+}
---
+++ src/w3schools/Strings/special_Characters.java
@@ -0,0 +1,12 @@
+package w3schools.Strings;
+
+public class special_Characters {
+    public static void main(String[] args) {
+        String txt = "We are the so-called \"Vikings\" from the north.";
+        String txt1 = "It\'s alright.";
+        String txt2 = "The character \\ is called backslash.";
+
+        String message = "cool";
+        System.out.println(message);
+    }
+}
