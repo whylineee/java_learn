0a8eacf3100db938513e1a264e7890dcb6d30647
whylineee <msavin901001@gmail.com>
2026-04-09 23:09:46 +0300

    start learn Short hand if else

--- src/w3schools/if_else.java
+++ src/w3schools/if_else.java
@@ -2,60 +2,97 @@ package w3schools;
 
 public class if_else {
     public static void main(String[] args) {
-      //  boolean issRaining = true;
+          //  boolean issRaining = true;
 
-//        if (issRaining) {
-//            System.out.println("Bring an umbrella!");
+    //        if (issRaining) {
+    //            System.out.println("Bring an umbrella!");
+    //
+    //        if (20 > 18) {
+    //            System.out.println("20 greater then 18");
+    //        }
+    //
+    //       // int x = 20;
+    //       // int y = 20;
+    //      //  if (x == y) {
+    //       //     System.out.println("das");
+    //     //   }
+    //
+    //        //boolean isLightOn = true;
+    //
+    //       // if (isLightOn) {
+    //        //    System.out.println("The light is on.");
+    //      //  }
+    //
+    //            boolean isLightOn = false;
+    //
+    //            if (isLightOn) {
+    //                System.out.println("The light is on.");  // This will not be printed
+    //            }
+    //
+    //            System.out.println("This line runs no matter what, because it is outside the if statement.");
+    //
+    //            int x = 20;
+    //            int y = 18;
+    //
+    //            if (x > y)
+    //                System.out.println("x is greater than y ");
+    //                System.out.println("This line runs no matter what ");
+
+    // =========================================================================
+                // ELSE //
+    //        boolean isRaining = false;
+    //
+    //        if (isRaining) {
+    //            System.out.println("Bring an umbrella!");
+    //        } else {
+    //            System.out.println("No rain today, no need for an umbrella!");
+    //        }
+    //
+    //        int time = 20;
+    //
+    //        if (time < 18) {
+    //            System.out.println("Good day.");
+    //        } else {
+    //            System.out.println("Good evening");
+    //        }
+    //
+    //
+
+
+
+    // ========================================================================
+    //          ELSE IF
+
+//        int weather = 2;
 //
-//        if (20 > 18) {
-//            System.out.println("20 greater then 18");
+//        if (weather == 1) {
+//            System.out.println("Bring an umbrella");
+//        } else if (weather == 2) {
+//            System.out.println("Wear sunglasses");
+//        } else {
+//            System.out.println("Just go outside normally.");
 //        }
 //
-//       // int x = 20;
-//       // int y = 20;
-//      //  if (x == y) {
-//       //     System.out.println("das");
-//     //   }
+//        int time = 16;
 //
-//        //boolean isLightOn = true;
+//        if (time < 12) {
+//            System.out.println("Good morning.");
+//        } else if (time < 18) {
+//            System.out.println("Good day.");
+//        } else {
+//            System.out.println("Good evening.");
+//        }
 //
-//       // if (isLightOn) {
-//        //    System.out.println("The light is on.");
-//      //  }
 //
-//            boolean isLightOn = false;
+//        int time = 22;
 //
-//            if (isLightOn) {
-//                System.out.println("The light is on.");  // This will not be printed
-//            }
-//
-//            System.out.println("This line runs no matter what, because it is outside the if statement.");
-//
-//            int x = 20;
-//            int y = 18;
-//
-//            if (x > y)
-//                System.out.println("x is greater than y ");
-//                System.out.println("This line runs no matter what ");
-
-// =========================================================================
-            // ELSE //
-        boolean isRaining = false;
-
-        if (isRaining) {
-            System.out.println("Bring an umbrella!");
-        } else {
-            System.out.println("No rain today, no need for an umbrella!");
-        }
-
-        int time = 20;
-
-        if (time < 18) {
-            System.out.println("Good day.");
-        } else {
-            System.out.println("Good evening");
-        }
-
+//        if (time < 12) {
+//            System.out.println("Good morning.");
+//        } else if (time < 18) {
+//            System.out.println("Good day.");
+//        } else {
+//            System.out.println("Good evening.");
+//        }
 
 
         }
