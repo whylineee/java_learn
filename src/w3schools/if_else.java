167aed17eda3460d6334ef3d7a43c15beaeb3c5f
whylineee <msavin901001@gmail.com>
2026-04-09 16:41:10 +0300

    end learn if else and start learn Else IF
    clear

--- src/w3schools/if_else.java
+++ src/w3schools/if_else.java
@@ -2,42 +2,61 @@ package w3schools;
 
 public class if_else {
     public static void main(String[] args) {
-        boolean isRaining = true;
+      //  boolean issRaining = true;
+
+//        if (issRaining) {
+//            System.out.println("Bring an umbrella!");
+//
+//        if (20 > 18) {
+//            System.out.println("20 greater then 18");
+//        }
+//
+//       // int x = 20;
+//       // int y = 20;
+//      //  if (x == y) {
+//       //     System.out.println("das");
+//     //   }
+//
+//        //boolean isLightOn = true;
+//
+//       // if (isLightOn) {
+//        //    System.out.println("The light is on.");
+//      //  }
+//
+//            boolean isLightOn = false;
+//
+//            if (isLightOn) {
+//                System.out.println("The light is on.");  // This will not be printed
+//            }
+//
+//            System.out.println("This line runs no matter what, because it is outside the if statement.");
+//
+//            int x = 20;
+//            int y = 18;
+//
+//            if (x > y)
+//                System.out.println("x is greater than y ");
+//                System.out.println("This line runs no matter what ");
+
+// =========================================================================
+            // ELSE //
+        boolean isRaining = false;
 
         if (isRaining) {
             System.out.println("Bring an umbrella!");
-
-        if (20 > 18) {
-            System.out.println("20 greater then 18");
+        } else {
+            System.out.println("No rain today, no need for an umbrella!");
         }
 
-       // int x = 20;
-       // int y = 20;
-      //  if (x == y) {
-       //     System.out.println("das");
-     //   }
+        int time = 20;
 
-        //boolean isLightOn = true;
+        if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening");
+        }
 
-       // if (isLightOn) {
-        //    System.out.println("The light is on.");
-      //  }
 
-            boolean isLightOn = false;
-
-            if (isLightOn) {
-                System.out.println("The light is on.");  // This will not be printed
-            }
-
-            System.out.println("This line runs no matter what, because it is outside the if statement.");
-
-            int x = 20;
-            int y = 18;
-
-            if (x > y)
-                System.out.println("x is greater than y ");
-                System.out.println("This line runs no matter what ");
 
         }
     }
-}
