d8eb4b9624579adc4c5d99257a9eae43c0f81b04
whylineee <msavin901001@gmail.com>
2026-04-09 16:09:05 +0300

    end learn if, start learn else and learn if else

--- src/w3schools/if_else.java
+++ src/w3schools/if_else.java
@@ -7,8 +7,36 @@ public class if_else {
         if (isRaining) {
             System.out.println("Bring an umbrella!");
 
+        if (20 > 18) {
+            System.out.println("20 greater then 18");
+        }
 
+       // int x = 20;
+       // int y = 20;
+      //  if (x == y) {
+       //     System.out.println("das");
+     //   }
 
+        //boolean isLightOn = true;
+
+       // if (isLightOn) {
+        //    System.out.println("The light is on.");
+      //  }
+
+            boolean isLightOn = false;
+
+            if (isLightOn) {
+                System.out.println("The light is on.");  // This will not be printed
+            }
+
+            System.out.println("This line runs no matter what, because it is outside the if statement.");
+
+            int x = 20;
+            int y = 18;
+
+            if (x > y)
+                System.out.println("x is greater than y ");
+                System.out.println("This line runs no matter what ");
 
         }
     }
